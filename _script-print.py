d0c08820a7bc1ddd24595dfef39ae0c951bf67b5
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-06-11 14:10:45 -0300

    Scripts

---
+++ _script-print.py
@@ -0,0 +1,2 @@
+print("Linha 1")
+print("Linha 2")
---
+++ _script.py
@@ -0,0 +1,9 @@
+import os
+
+# Comando para executar o primeiro arquivo Python
+os.system("python _script-flower.py 2> _script-flower.txt")
+# os.system("python _script-flower.py | grep -A 1000 'INFO : \[SUMMARY\]' > _script-flower-sumary.txt")
+
+
+
+

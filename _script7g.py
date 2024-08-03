e49377172e1cb58fc6af5df2de7b2be3489b452e
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-03 19:26:06 -0300

    Resultados

--- _script7g.py
+++ _script7g.py
@@ -12,18 +12,18 @@ import os
 
 # os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02-1.txt")
 # os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02-2.txt")
-# os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02-3.txt")
+os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda02-3.txt")
 
-os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04-1.txt")
+# os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04-1.txt")
 # os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04-2.txt")
 os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda04-3.txt")
 
 # os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06-1.txt")
-os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06-2.txt")
+# os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06-2.txt")
 os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda06-3.txt")
 
 os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08-1.txt")
-os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08-2.txt")
+# os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08-2.txt")
 os.system("python FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08.py 2> FedRecSysFair-FedCustom-LossGroup-Activity_7g-lambda08-3.txt")
 
 # AGE ----------------------------------------------------------

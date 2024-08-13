4c35605b7565dad7eeaaf8d29b656318dc672a27
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-13 19:36:48 -0300

    Resultados

--- _script.py
+++ _script.py
@@ -6,16 +6,20 @@ import os
 # os.system("python FedRecSysFair-FedAVG.py 2> FedRecSysFair-FedAVG-4.txt")
 
 # os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-1.txt")
-os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-2.txt")
-os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-3.txt")
-os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-4.txt")
+# os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-2.txt")
+# os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-3.txt")
+# os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-4.txt")
+os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-5.txt")
+os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-6.txt")
 
 # # ACTIVITY
 
 # os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-1.txt")
-os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-2.txt")
-os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-3.txt")
-os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-4.txt")
+# os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-2.txt")
+# os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-3.txt")
+# os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-4.txt")
+os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-5.txt")
+os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-6.txt")
 
 # # AGE ----------------------------------------------------------
 

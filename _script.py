bfba244e156ed16bb32f6ef8a39dfc45bf88bf90
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-13 19:50:52 -0300

    Lambdas

--- _script.py
+++ _script.py
@@ -1,28 +1,41 @@
 import os
 
+# FedAvg --------------------------------------------------------------------------------------
 # os.system("python FedRecSysFair-FedAVG.py 2> FedRecSysFair-FedAVG-1.txt")
 # os.system("python FedRecSysFair-FedAVG.py 2> FedRecSysFair-FedAVG-2.txt")
 # os.system("python FedRecSysFair-FedAVG.py 2> FedRecSysFair-FedAVG-3.txt")
 # os.system("python FedRecSysFair-FedAVG.py 2> FedRecSysFair-FedAVG-4.txt")
 
+# Fed(l) --------------------------------------------------------------------------------------
 # os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-1.txt")
 # os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-2.txt")
 # os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-3.txt")
 # os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-4.txt")
-os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-5.txt")
-os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-6.txt")
+# os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-5.txt")
+# os.system("python FedRecSysFair-FedLoss.py 2> FedRecSysFair-FedLoss-6.txt")
 
-# # ACTIVITY
-
+# FairFed(l) ACTIVITY -------------------------------------------------------------------------
 # os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-1.txt")
 # os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-2.txt")
 # os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-3.txt")
 # os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-4.txt")
-os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-5.txt")
-os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-6.txt")
+# os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-5.txt")
+# os.system("python FedRecSysFair-FairFed-Activity.py 2> FedRecSysFair-FairFed-Activity-6.txt")
 
-# # AGE ----------------------------------------------------------
+# FairFed(l) AGE ------------------------------------------------------------------------------
+os.system("python FedRecSysFair-FairFed-Age.py 2> FedRecSysFair-FairFed-Age-1.txt")
+os.system("python FedRecSysFair-FairFed-Age.py 2> FedRecSysFair-FairFed-Age-2.txt")
+os.system("python FedRecSysFair-FairFed-Age.py 2> FedRecSysFair-FairFed-Age-3.txt")
+os.system("python FedRecSysFair-FairFed-Age.py 2> FedRecSysFair-FairFed-Age-4.txt")
+os.system("python FedRecSysFair-FairFed-Age.py 2> FedRecSysFair-FairFed-Age-5.txt")
+os.system("python FedRecSysFair-FairFed-Age.py 2> FedRecSysFair-FairFed-Age-6.txt")
 
-
+# FairFed(l) GENDER ---------------------------------------------------------------------------
+os.system("python FedRecSysFair-FairFed-Gender.py 2> FedRecSysFair-FairFed-Gender-1.txt")
+os.system("python FedRecSysFair-FairFed-Gender.py 2> FedRecSysFair-FairFed-Gender-2.txt")
+os.system("python FedRecSysFair-FairFed-Gender.py 2> FedRecSysFair-FairFed-Gender-3.txt")
+os.system("python FedRecSysFair-FairFed-Gender.py 2> FedRecSysFair-FairFed-Gender-4.txt")
+os.system("python FedRecSysFair-FairFed-Gender.py 2> FedRecSysFair-FairFed-Gender-5.txt")
+os.system("python FedRecSysFair-FairFed-Gender.py 2> FedRecSysFair-FairFed-Gender-6.txt")
 
 

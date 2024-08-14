d9c00d0a1416a045056f409d1ba028b59ff2c660
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-14 08:20:02 -0300

    Resultados

---
+++ _script copy.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-1.txt")
+os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-2.txt")
+os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-3.txt")
+
+
+
+
+

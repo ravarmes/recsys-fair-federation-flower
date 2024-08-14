2e8fcbbdc1cbbcb6740b86ba926b04cae5aa046d
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-14 10:00:38 -0300

    Resultados

--- _script copy.py
+++ _script copy.py
@@ -1,8 +1,8 @@
 import os
 
-os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-1.txt")
-os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-2.txt")
-os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-3.txt")
+os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-4.txt")
+os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-5.txt")
+os.system("python FedRecSysFair-FairFed-Activity-Fair0.py 2> FedRecSysFair-FairFed-Activity-Fair0-6.txt")
 
 
 

8a2906eaac03852fb0fa402739715f758b5d003e
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-15 10:43:11 -0300

    Script

--- _script-aggregate.py
+++ _script-aggregate.py
@@ -8,5 +8,11 @@ import os
 # os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-2.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateLoss.py 2> FedRecSysFair-FedCustom-AggregateLoss-2.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateMedian.py 2> FedRecSysFair-FedCustom-AggregateMedian-2.txt")
-os.system("python FedRecSysFair-FedCustom-AggregateWeightedLossAvg.py 2> FedRecSysFair-FedCustom-AggregateWeightedLossAvg-2.txt")
+# os.system("python FedRecSysFair-FedCustom-AggregateWeightedLossAvg.py 2> FedRecSysFair-FedCustom-AggregateWeightedLossAvg-2.txt")
+
+os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-1.txt")
+os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-2.txt")
+os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-3.txt")
+
+
 

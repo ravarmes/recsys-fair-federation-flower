085d16d232da56f4d84f7712cb987d0b71e490ea
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-16 08:32:11 -0300

    Estratégias de Agregação

--- _script-aggregate.py
+++ _script-aggregate.py
@@ -1,18 +1,13 @@
 import os
 
-os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-1.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateLoss.py 2> FedRecSysFair-FedCustom-AggregateLoss-1.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateMedian.py 2> FedRecSysFair-FedCustom-AggregateMedian-1.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateWeightedLossAvg.py 2> FedRecSysFair-FedCustom-AggregateWeightedLossAvg-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Example.py 2> FedRecSysFair-FedCustom-Aggregate-Example-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Example-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-1.txt")
+os.system("python FedRecSysFair-FedCustom-AggregateInplace-Example.py 2> FedRecSysFair-FedCustom-AggregateInplace-Example-1.txt")
+os.system("python FedRecSysFair-FedCustom-AggregateInplace-Loss.py 2> FedRecSysFair-FedCustom-AggregateInplace-Loss-1.txt")
 
-os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-2.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateLoss.py 2> FedRecSysFair-FedCustom-AggregateLoss-2.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateMedian.py 2> FedRecSysFair-FedCustom-AggregateMedian-2.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateWeightedLossAvg.py 2> FedRecSysFair-FedCustom-AggregateWeightedLossAvg-2.txt")
-
-# os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-1.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-2.txt")
-# os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-3.txt")
-
-
-
+os.system("python FedRecSysFair-FedCustom-Aggregate-Example.py 2> FedRecSysFair-FedCustom-Aggregate-Example-2.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Example-2.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-2.txt")
+os.system("python FedRecSysFair-FedCustom-AggregateInplace-Example.py 2> FedRecSysFair-FedCustom-AggregateInplace-Example-2.txt")
+os.system("python FedRecSysFair-FedCustom-AggregateInplace-Loss.py 2> FedRecSysFair-FedCustom-AggregateInplace-Loss-2.txt")

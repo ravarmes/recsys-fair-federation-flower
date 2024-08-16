90f4a24bd66b7f4a121c580d8ce8e91bd157fef4
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-15 22:33:42 -0300

    Resultados

--- _script-aggregate.py
+++ _script-aggregate.py
@@ -1,11 +1,11 @@
 import os
 
-# os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-1.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateLoss.py 2> FedRecSysFair-FedCustom-AggregateLoss-1.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateMedian.py 2> FedRecSysFair-FedCustom-AggregateMedian-1.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateWeightedLossAvg.py 2> FedRecSysFair-FedCustom-AggregateWeightedLossAvg-1.txt")
 
-# os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-2.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate.py 2> FedRecSysFair-FedCustom-Aggregate-2.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateLoss.py 2> FedRecSysFair-FedCustom-AggregateLoss-2.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateMedian.py 2> FedRecSysFair-FedCustom-AggregateMedian-2.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateWeightedLossAvg.py 2> FedRecSysFair-FedCustom-AggregateWeightedLossAvg-2.txt")
@@ -14,7 +14,5 @@ import os
 # os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-2.txt")
 # os.system("python FedRecSysFair-FedCustom-AggregateLoss-Fair.py 2> FedRecSysFair-FedCustom-AggregateLoss-Fair-3.txt")
 
-os.system("python FedRecSysFair-FedCustom-AggregateAvg.py 2> FedRecSysFair-FedCustom-AggregateAvg-1.txt")
-os.system("python FedRecSysFair-FedCustom-AggregateAvg.py 2> FedRecSysFair-FedCustom-AggregateAvg-2.txt")
-os.system("python FedRecSysFair-FedCustom-AggregateAvg.py 2> FedRecSysFair-FedCustom-AggregateAvg-3.txt")
+
 

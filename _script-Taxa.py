c0d6b5f7e658c26b4e9473db2f2fd39eee4abb57
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-17 15:44:54 -0300

    Resultados

---
+++ _script-Taxa.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa1.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa1-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa2.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa2-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa3.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa3-1.txt")
+
+

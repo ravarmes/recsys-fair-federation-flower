ff9dc628b7cfd75d726e3579a53bebaaa1587a9b
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-17 19:16:33 -0300

    Scripts

--- _script-Taxa.py
+++ _script-Taxa.py
@@ -5,9 +5,15 @@ import os
 # os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa3.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa3-1.txt")
 
 # os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4-1.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4-2.txt")
+# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4-2.txt")
 
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5-1.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5-2.txt")
+# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5-1.txt")
+# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5-2.txt")
+
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa6-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa4.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa6-2.txt")
+
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa7-1.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa5.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Taxa7-2.txt")
 
 

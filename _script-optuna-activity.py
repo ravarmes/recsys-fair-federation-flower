4164680bf6653a9b1d29cbf10934dd3e591b451f
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-08-26 13:36:14 -0300

    Optuna

---
+++ _script-optuna-activity.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_03.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_03-01.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_03.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_03-02.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_03.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_03-03.txt")

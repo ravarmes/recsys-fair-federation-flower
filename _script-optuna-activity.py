796a428afbcf75be59e25196c6ecb9706e87022e
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-09-05 14:59:10 -0300

    Resultados

--- _script-optuna-activity.py
+++ _script-optuna-activity.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_04.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_04-01.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_04.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_04-02.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_04.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_04-03.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_05_Sqtr-01.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_05_Sqtr-02.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Activity-Optuna_05_Sqtr-03.txt")

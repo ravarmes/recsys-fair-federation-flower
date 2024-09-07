22260dc34713f264cf289e374779c10d544ce8d4
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-09-07 13:42:35 -0300

    Resultados

---
+++ _script-optuna-age.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-02.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-03.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-04.txt")
+os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-05.txt")

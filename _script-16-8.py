f0043818947b95ffd3f340d2040f6dc7afee87ea
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-09-26 07:44:05 -0300

    Resultados

--- _script-16-8.py
+++ _script-16-8.py
@@ -1,7 +1,7 @@
 import os
 
 # FedAvg --------------------------------------------------------------------------------------
-os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-1.txt")
+# os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-1.txt")
 os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-2.txt")
 # os.system("python FedRecSysFair-FedCustom-Aggregate-Example.py 2> FedRecSysFair-FedCustom-Aggregate-Example-3.txt")
 

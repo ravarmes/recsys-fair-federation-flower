f21f068ba3c839166ad516dd07ad749554365768
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-01 19:45:56 -0300

    Resultados

--- _script-gradiente.py
+++ _script-gradiente.py
@@ -1,11 +1,11 @@
 import os
 
-os.system("python FedFair-Loss-Activity-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-01.txt")
-os.system("python FedFair-Loss-Activity-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-02.txt")
+# os.system("python FedFair-Loss-Activity-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-06.txt")
+# os.system("python FedFair-Loss-Activity-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-07.txt")
 
-os.system("python FedFair-Loss-Age-Gradiente-Sqrt.py 2> FedFair-Loss-Age-Gradiente-Sqrt-01.txt")
-os.system("python FedFair-Loss-Age-Gradiente-Sqrt.py 2> FedFair-Loss-Age-Gradiente-Sqrt-02.txt")
+os.system("python FedFair-Loss-Age-Gradiente-Sqrt.py 2> FedFair-Loss-Age-Gradiente-Sqrt-05.txt")
+os.system("python FedFair-Loss-Age-Gradiente-Sqrt.py 2> FedFair-Loss-Age-Gradiente-Sqrt-06.txt")
 
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-01.txt")
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-02.txt")
+os.system("python FedFair-Loss-Gender-Gradiente-Sqrt.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-05.txt")
+os.system("python FedFair-Loss-Gender-Gradiente-Sqrt.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-06.txt")
 

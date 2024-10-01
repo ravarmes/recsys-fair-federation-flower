86be026403b40e9a3240f3a0638186219e151136
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-01 07:44:35 -0300

    Resultados

---
+++ _script-gradiente.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-01.txt")
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-02.txt")
+
+os.system("python FedFair-Loss-Age-Gradiente-Sqrt.py 2> FedFair-Loss-Age-Gradiente-Sqrt-01.txt")
+os.system("python FedFair-Loss-Age-Gradiente-Sqrt.py 2> FedFair-Loss-Age-Gradiente-Sqrt-02.txt")
+
+os.system("python FedFair-Loss-Gender-Gradiente-Sqrt.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-01.txt")
+os.system("python FedFair-Loss-Gender-Gradiente-Sqrt.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-02.txt")
+

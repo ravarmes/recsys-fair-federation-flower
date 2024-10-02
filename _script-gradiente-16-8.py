ade917f5488b265925c40e29716f3eeb9fc7c328
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-02 13:28:21 -0300

    Resultados

---
+++ _script-gradiente-16-8.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("python FedFair-Loss-Activity-16-8-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-16-8-Gradiente-Sqrt-01.txt")
+os.system("python FedFair-Loss-Activity-16-8-Gradiente-Sqrt.py 2> FedFair-Loss-Activity-16-8-Gradiente-Sqrt-02.txt")
+

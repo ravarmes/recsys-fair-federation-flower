2377641dcb034169650a68b94630db76ae5f3b98
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-06 06:46:39 -0300

    Resultados

--- FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py
+++ FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py
@@ -615,7 +615,7 @@ if DEVICE.type == "cuda":
 fl.simulation.start_simulation(
     client_fn=client_fn,
     num_clients=NUM_CLIENTS,
-    config=fl.server.ServerConfig(num_rounds=2),
+    config=fl.server.ServerConfig(num_rounds=24),
     strategy=FedCustom(),
     client_resources=client_resources,
 )

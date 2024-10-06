68d093bee90e8f16a0e1f11df5a914aafa23cd0c
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-06 06:46:57 -0300

    Resultados

--- _script-16-8.py
+++ _script-16-8.py
@@ -1,12 +1,13 @@
 import os
 
 # FedAvg --------------------------------------------------------------------------------------
-os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-07.txt")
-# os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-04.txt")
+os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-08.txt")
+os.system("python FedAvg-Example-16-8.py 2> FedAvg-Example-16-8-09.txt")
 
 # Fed(l) --------------------------------------------------------------------------------------
-# os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-07.txt")
-# os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-04.txt")
+os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-07.txt")
+os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-08.txt")
+os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-09.txt")
 
 # # FairFed(l) ACTIVITY -------------------------------------------------------------------------
 # os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-06.txt")

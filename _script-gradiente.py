f9f3ee28500e6e41b03c224996fe003365629036
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-08 07:57:48 -0300

    Resultados

--- _script-gradiente.py
+++ _script-gradiente.py
@@ -10,9 +10,8 @@ import os
 # os.system("python FedFair-Loss-Gender-Gradiente.py 2> FedFair-Loss-Gender-Gradiente-Taxa040-02.txt")
 
 
-os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-01.txt")
-os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-02.txt")
-os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-03.txt")
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-43.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-43-01.txt")
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-43.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-43-02.txt")
 # os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-5.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-5-03.txt")
 # os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-5.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-5-04.txt")
 # os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-5.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-5-05.txt")

9eb442e5496bf115b893ed5f88b7c62f549387d5
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-10 21:03:09 -0300

    Resultados

--- _script-gradiente_group.py
+++ _script-gradiente_group.py
@@ -1,18 +1,14 @@
 import os
 
-# os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group-07.txt")
-# os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group-08.txt")
-# os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group-09.txt")
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group-10.txt")
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group-11.txt")
+os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-02-Group-12.txt")
 
-os.system("python FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group-08.txt")
-os.system("python FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group-09.txt")
-os.system("python FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group-10.txt")
+os.system("python FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group-11.txt")
+os.system("python FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Age-Gradiente-Sqrt-Taxa-02-Group-12.txt")
 
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-06.txt")
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-07.txt")
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-08.txt")
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-09.txt")
-os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-10.txt")
+os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-11.txt")
+os.system("python FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-Gradiente-Sqrt-Taxa-02-Group-12.txt")
 
 
 # os.system("python FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-373-Group.py 2> FedFair-Loss-Activity-Gradiente-Sqrt-Taxa-373-Group-01.txt")

6b7f215c7d8450877770469a3bfb409a0ed35a35
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-20 18:46:13 -0300

    Resultados

--- _script.py
+++ _script.py
@@ -1,16 +1,16 @@
 import os
 
 # FedAvg --------------------------------------------------------------------------------------
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Example.py 2> FedRecSysFair-FedCustom-Aggregate-Example-5.txt")
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Example.py 2> FedRecSysFair-FedCustom-Aggregate-Example-6.txt")
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Example.py 2> FedRecSysFair-FedCustom-Aggregate-Example-3.txt")
+os.system("python FedAvg-Example.py 2> FedAvg-Example-06.txt")
+os.system("python FedAvg-Example.py 2> FedAvg-Example-07.txt")
+os.system("python FedAvg-Example.py 2> FedAvg-Example-08.txt")
+os.system("python FedAvg-Example.py 2> FedAvg-Example-09.txt")
+os.system("python FedAvg-Example.py 2> FedAvg-Example-10.txt")
 
 # Fed(l) --------------------------------------------------------------------------------------
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-17.txt")
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-18.txt")
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-19.txt")
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-20.txt")
-# os.system("python FedRecSysFair-FedCustom-Aggregate-Loss.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-21.txt")
+os.system("python FedAvg-Loss.py 2> FedAvg-Loss-08.txt")
+os.system("python FedAvg-Loss.py 2> FedAvg-Loss-09.txt")
+os.system("python FedAvg-Loss.py 2> FedAvg-Loss-10.txt")
 
 
 # # FairFed(l) ACTIVITY -------------------------------------------------------------------------
@@ -33,15 +33,15 @@ import os
 # os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Gender.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Gender-20.txt")
 
 
-os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-01.txt")
-os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-02.txt")
-os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-03.txt")
-os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-04.txt")
-os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-05.txt")
+# os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-01.txt")
+# os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-02.txt")
+# os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-03.txt")
+# os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-04.txt")
+# os.system("python FedFair-Loss-Activity-Sqrt.py 2> FedFair-Loss-Activity-Sqrt-05.txt")
 
-os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-01.txt")
-os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-02.txt")
-os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-03.txt")
-os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-04.txt")
-os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-05.txt")
+# os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-01.txt")
+# os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-02.txt")
+# os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-03.txt")
+# os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-04.txt")
+# os.system("python FedFair-Loss-Activity.py 2> FedFair-Loss-Activity-05.txt")
 

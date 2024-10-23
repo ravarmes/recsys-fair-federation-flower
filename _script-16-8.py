e4be1b24109c449f4c225a1e3683171b373ab9bb
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-23 06:22:24 -0300

    Resultados

--- _script-16-8.py
+++ _script-16-8.py
@@ -9,16 +9,19 @@ import os
 # os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-18.txt")
 
 # FairFed(l) ACTIVITY -------------------------------------------------------------------------
-os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-09.txt")
-os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-10.txt")
+os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-11.txt")
+os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-12.txt")
+os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-13.txt")
+os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-14.txt")
+
 
 # FairFed(l) AGE ------------------------------------------------------------------------------
-os.system("python FedFair-Loss-Age-16-8.py 2> FedFair-Loss-Age-16-8-09.txt")
-os.system("python FedFair-Loss-Age-16-8.py 2> FedFair-Loss-Age-16-8-10.txt")
+# os.system("python FedFair-Loss-Age-16-8.py 2> FedFair-Loss-Age-16-8-09.txt")
+# os.system("python FedFair-Loss-Age-16-8.py 2> FedFair-Loss-Age-16-8-10.txt")
 
 # FairFed(l) GENDER ---------------------------------------------------------------------------
-os.system("python FedFair-Loss-Gender-16-8.py 2> FedFair-Loss-Gender-16-8.py-09.txt")
-os.system("python FedFair-Loss-Gender-16-8.py 2> FedFair-Loss-Gender-16-8.py-10.txt")
+# os.system("python FedFair-Loss-Gender-16-8.py 2> FedFair-Loss-Gender-16-8.py-09.txt")
+# os.system("python FedFair-Loss-Gender-16-8.py 2> FedFair-Loss-Gender-16-8.py-10.txt")
 
 # # FairFed(l) ACTIVITY -------------------------------------------------------------------------
 # os.system("python FedFair-Loss-Activity-16-8-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Activity-16-8-Gradiente-Sqrt-Taxa-02-Group-03.txt")

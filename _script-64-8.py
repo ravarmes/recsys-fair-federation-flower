d6eb311a8f7be9fd98c6ef1c56521dbae6bae314
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-27 08:53:38 -0300

    Resultados

--- _script-64-8.py
+++ _script-64-8.py
@@ -22,17 +22,17 @@ import os
 
 
 # FairFed(l) AGE ------------------------------------------------------------------------------
-os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-01.txt")
-os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-02.txt")
-os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-03.txt")
-os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-04.txt")
-os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-05.txt")
+# os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-06.txt")
+# os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-07.txt")
+# os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-08.txt")
+# os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-09.txt")
+# os.system("python FedFair-Loss-Age-64-8.py 2> FedFair-Loss-Age-64-8-10.txt")
 
 # FairFed(l) GENDER ---------------------------------------------------------------------------
-os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-01.txt")
-os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-02.txt")
-os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-03.txt")
-os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-04.txt")
-os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-05.txt")
+os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-06.txt")
+os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-07.txt")
+os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-08.txt")
+os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-09.txt")
+os.system("python FedFair-Loss-Gender-64-8.py 2> FedFair-Loss-Gender-64-8-10.txt")
 
 # # FairFed(l) ACTIVITY --------------------------------------------------------------

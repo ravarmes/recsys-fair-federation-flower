324f7c4f52cd34e791f36b92971cf8f7984adb8f
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-10-28 20:43:55 -0300

    Resultados

--- FedAvg-Loss-64-8-DEEV-Maiores.py
+++ FedAvg-Loss-64-8-DEEV-Maiores.py
@@ -560,7 +560,7 @@ if DEVICE.type == "cuda":
 fl.simulation.start_simulation(
     client_fn=client_fn,
     num_clients=NUM_CLIENTS,
-    config=fl.server.ServerConfig(num_rounds=24),
+    config=fl.server.ServerConfig(num_rounds=12),
     strategy=FedCustom(),
     client_resources=client_resources,
 )

e5bfad2b45df905dab60d23610d67760bc1f3fb3
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-06 18:36:21 -0300

    Script

--- _script-64-8.py
+++ _script-64-8.py
@@ -6,11 +6,11 @@ os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-12.txt")
 os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-13.txt")
 os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-14.txt")
 os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-15.txt")
-os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-16.txt")
-os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-17.txt")
-os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-18.txt")
-os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-19.txt")
-os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-20.txt")
+# os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-16.txt")
+# os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-17.txt")
+# os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-18.txt")
+# os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-19.txt")
+# os.system("python FedAvg-Example-64-8.py 2> FedAvg-Example-64-8-20.txt")
 
 
 # FedAvg Loss ---------------------------------------------------------------------------------

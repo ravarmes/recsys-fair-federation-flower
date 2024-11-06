91d699f589cb45e51bfbe6a59c98cd3c3845ace4
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-06 18:01:39 -0300

    Script

--- _script-optuna-age.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-11.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-12.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-13.txt")
-os.system("python FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr.py 2> FedRecSysFair-FedCustom-Aggregate-Loss-Fair-Age-Optuna_05_Sqtr-14.txt")
--- _script-scale.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-os.system("python FedFair-Loss-Activity-Scale.py 2> FedFair-Loss-Activity-Scale-01.txt")
-os.system("python FedFair-Loss-Activity-Scale.py 2> FedFair-Loss-Activity-Scale-02.txt")
-os.system("python FedFair-Loss-Activity-Scale.py 2> FedFair-Loss-Activity-Scale-03.txt")
-os.system("python FedFair-Loss-Activity-Scale.py 2> FedFair-Loss-Activity-Scale-04.txt")
-os.system("python FedFair-Loss-Activity-Scale.py 2> FedFair-Loss-Activity-Scale-05.txt")
-

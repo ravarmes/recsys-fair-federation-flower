a7913ed74334a9fcfcf5bd8bf96694b3ac881d50
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-08 18:24:13 -0300

    Resultados

--- _script-16-8.py
+++ _script-16-8.py
@@ -9,8 +9,8 @@ import os
 # os.system("python FedAvg-Loss-16-8.py 2> FedAvg-Loss-16-8-20.txt")
 
 # FairFed(l) ACTIVITY -------------------------------------------------------------------------
-os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-19.txt")
-os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-20.txt")
+# os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-19.txt")
+# os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-20.txt")
 
 
 # FairFed(l) AGE ------------------------------------------------------------------------------
@@ -35,3 +35,4 @@ os.system("python FedFair-Loss-Activity-16-8.py 2> FedFair-Loss-Activity-16-8-20
 # os.system("python FedFair-Loss-Age-16-8-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Age-16-8-Gradiente-Sqrt-Taxa-02-Group-02.txt")
 # os.system("python FedFair-Loss-Gender-16-8-Gradiente-Sqrt-Taxa-02-Group.py 2> FedFair-Loss-Gender-16-8-Gradiente-Sqrt-Taxa-02-Group-02.txt")
 
+os.system("python FedDEEV-Loss-Age-16-8.py 2> FedDEEV-Loss-Age-16-8-02.txt")

b8f2d09fcc06b41c01a2e03c2aaf495c047d3fed
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-08 09:29:53 -0300

    script

---
+++ _script-32-16.py
@@ -0,0 +1,16 @@
+import os
+
+# FedDEEV AGE---- --------------------------------------------------------------------------
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-01.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-02.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-03.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-04.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-05.txt")
+
+
+# FedDEEV GENDER --------------------------------------------------------------------------
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-01.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-02.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-03.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-04.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-05.txt")

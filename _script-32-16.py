bca4caa99717a9a582fdf99eab7e2538aa618a0a
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-08 18:23:05 -0300

    Resultados

--- _script-32-16.py
+++ _script-32-16.py
@@ -1,16 +1,16 @@
 import os
 
 # FedDEEV AGE---- --------------------------------------------------------------------------
-# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-01.txt")
-# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-02.txt")
-# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-03.txt")
-# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-04.txt")
-# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-05.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-06.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-07.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-08.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-09.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-10.txt")
 
 
 # FedDEEV GENDER --------------------------------------------------------------------------
-# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-01.txt")
-# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-02.txt")
-# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-03.txt")
-# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-04.txt")
-# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-05.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-06.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-07.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-08.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-09.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-10.txt")

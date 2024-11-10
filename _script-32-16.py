f15e4a0f7615cd4097b8786f964f1656f3baa83b
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-10 05:13:21 -0300

    Resultados

--- _script-32-16.py
+++ _script-32-16.py
@@ -1,16 +1,16 @@
 import os
 
 # FedDEEV AGE---- --------------------------------------------------------------------------
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-06.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-07.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-08.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-09.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-10.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-16.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-17.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-18.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-19.txt")
+os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-20.txt")
 
 
 # FedDEEV GENDER --------------------------------------------------------------------------
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-06.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-07.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-08.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-09.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-10.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-16.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-17.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-18.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-19.txt")
+os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-20.txt")

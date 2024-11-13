5e4a68c4f181d0ba2916cd7fa8cfb71443cc28d3
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-13 10:07:28 -0300

    Resultados

--- _script-32-16.py
+++ _script-32-16.py
@@ -1,16 +1,19 @@
 import os
 
-# FedDEEV AGE---- --------------------------------------------------------------------------
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-16.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-17.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-18.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-19.txt")
-os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-20.txt")
+# FedDEEV ACTIVITY---- --------------------------------------------------------------------------
+os.system("python FedDEEV-Loss-Activity-32-16.py 2> FedDEEV-Loss-Activity-32-16-19.txt")
+os.system("python FedDEEV-Loss-Activity-32-16.py 2> FedDEEV-Loss-Activity-32-16-20.txt")
 
+# FedDEEV AGE---- --------------------------------------------------------------------------
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-16.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-17.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-18.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-19.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-20.txt")
 
 # FedDEEV GENDER --------------------------------------------------------------------------
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-16.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-17.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-18.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-19.txt")
-os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-20.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-16.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-17.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-18.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-19.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-20.txt")

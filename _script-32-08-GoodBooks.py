9dbfd56f04705d405090fb87d5b76cd4dfa07ffc
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-16 21:20:05 -0300

    Resultados

--- _script-32-08-GoodBooks.py
+++ _script-32-08-GoodBooks.py
@@ -1,13 +1,19 @@
 import os
 
 # # FedAvg --------------------------------------------------------------------------------------
-os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-03.txt")
-os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-04.txt")
-                  
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-09.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-10.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-11.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-12.txt")
+
 # # Fed(l) --------------------------------------------------------------------------------------
-os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-03.txt")
-os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-04.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-09.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-10.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-11.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-12.txt")
 
 # FairFed(l) ACTIVITY -------------------------------------------------------------------------
-os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-03.txt")
-os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-04.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-09.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-10.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-11.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-12.txt")

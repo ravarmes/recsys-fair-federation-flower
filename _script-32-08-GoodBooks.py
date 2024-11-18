ed868cc26d3e538fe2593861bc22d6f49e029d19
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-11-18 08:22:49 -0300

    Resultados

--- _script-32-08-GoodBooks.py
+++ _script-32-08-GoodBooks.py
@@ -1,19 +1,19 @@
 import os
 
 # # FedAvg --------------------------------------------------------------------------------------
-os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-13.txt")
-os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-14.txt")
-os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-15.txt")
-os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-16.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-17.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-18.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-19.txt")
+os.system("python FedAvg-Example-32-08-GoodBooks.py 2> FedAvg-Example-32-08-GoodBooks-20.txt")
 
 # # Fed(l) --------------------------------------------------------------------------------------
-os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-13.txt")
-os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-14.txt")
-os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-15.txt")
-os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-16.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-17.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-18.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-19.txt")
+os.system("python FedAvg-Loss-32-08-GoodBooks.py 2> FedAvg-Loss-32-08-GoodBooks-20.txt")
 
 # FairFed(l) ACTIVITY -------------------------------------------------------------------------
-os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-13.txt")
-os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-14.txt")
-os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-15.txt")
-os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-16.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-17.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-18.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-19.txt")
+os.system("python FedFair-Loss-Activity-32-08-GoodBooks.py 2> FedFair-Loss-Activity-32-08-GoodBooks-20.txt")

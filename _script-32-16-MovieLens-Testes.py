1421847e07ac7d87e46f2ba6b3c88c8c7cf2f444
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-12-12 10:15:17 -0300

    Novos scripts

---
+++ _script-32-16-MovieLens-Testes.py
@@ -0,0 +1,24 @@
+import os
+
+# FedDEEV ACTIVITY---- --------------------------------------------------------------------------
+os.system("python FedFair-Loss-Activity-32-16-PesoFixoGrupo.py 2> FedFair-Loss-Activity-32-16-PesoFixoGrupo-32-16-01.txt")
+os.system("python FedFair-Loss-Activity-32-16-PesoFixoGrupo.py 2> FedFair-Loss-Activity-32-16-PesoFixoGrupo-32-16-02.txt")
+
+os.system("python FedFair-Loss-Activity-32-16-Inverter.py 2> FedFair-Loss-Activity-32-16-Inverter-32-16-01.txt")
+os.system("python FedFair-Loss-Activity-32-16-Inverter.py 2> FedFair-Loss-Activity-32-16-Inverter-32-16-02.txt")
+
+
+
+# FedDEEV AGE---- --------------------------------------------------------------------------
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-16.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-17.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-18.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-19.txt")
+# os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-20.txt")
+
+# FedDEEV GENDER --------------------------------------------------------------------------
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-16.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-17.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-18.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-19.txt")
+# os.system("python FedDEEV-Loss-Gender-32-16.py 2> FedDEEV-Loss-Gender-32-16-20.txt")

16eb7fec75935ece4fdc1d35e3c950962a0fc054
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-12-12 10:15:50 -0300

    Resultados

--- _script-32-08-MovieLens.py
+++ _script-32-08-MovieLens.py
@@ -1,9 +1,9 @@
 import os
 
 # # FedAvg --------------------------------------------------------------------------------------
-os.system("python FedAvg-Example-32-08.py 2> FedAvg-Example-32-08-03.txt")
-os.system("python FedAvg-Example-32-08.py 2> FedAvg-Example-32-08-04.txt")
-os.system("python FedAvg-Example-32-08.py 2> FedAvg-Example-32-08-05.txt")
+# os.system("python FedAvg-Example-32-08.py 2> FedAvg-Example-32-08-03.txt")
+# os.system("python FedAvg-Example-32-08.py 2> FedAvg-Example-32-08-04.txt")
+# os.system("python FedAvg-Example-32-08.py 2> FedAvg-Example-32-08-05.txt")
 
 # # Fed(l) --------------------------------------------------------------------------------------
 os.system("python FedAvg-Loss-32-08.py 2> FedAvg-Loss-32-08-03.txt")

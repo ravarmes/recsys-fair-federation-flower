d817c74e44931fca5858a41f8bcabd9c9ea422e3
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2024-12-15 22:45:39 -0300

    Resultados

--- _script-32-16-MovieLens.py
+++ _script-32-16-MovieLens.py
@@ -1,8 +1,8 @@
 import os
 
 # FedDEEV ACTIVITY---- --------------------------------------------------------------------------
-os.system("python FedFair-Loss-Activity-32-16-Fator-Soma.py 2> FedFair-Loss-Activity-32-16-Fator-Soma-01.txt")
-os.system("python FedFair-Loss-Activity-32-16-Fator-Soma.py 2> FedFair-Loss-Activity-32-16-Fator-Soma-02.txt")
+os.system("python FedFair-Loss-Activity-32-16-Fator-Soma.py 2> FedFair-Loss-Activity-32-16-Fator-Soma-03.txt")
+os.system("python FedFair-Loss-Activity-32-16-Fator-Soma.py 2> FedFair-Loss-Activity-32-16-Fator-Soma-04.txt")
 
 # FedDEEV AGE---- --------------------------------------------------------------------------
 # os.system("python FedDEEV-Loss-Age-32-16.py 2> FedDEEV-Loss-Age-32-16-16.txt")
